fab427b9896c194c19a5026355bd957b1d64280d
Aleksander Aleksiev <aleksander.aleksiev12@abv.bg>
2020-02-07 10:25:19 +0200

    First commit

---
+++ HyperPoller/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HyperPoller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
